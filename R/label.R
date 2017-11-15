2a3f3a34ecbfd2f91cb8c7e48aa7daf7f57475e3
Jonathan Sidi <yonicd@gmail.com>
2017-11-15 09:47:07 -0500

    update description and add labels documentation

--- R/label.R
+++ R/label.R
@@ -1,3 +1,17 @@
+#' @title Add label attribute to data.frame columns
+#' @description Utility function to add label attribute to data.frame columns.
+#' @param .data data.frame, data.frame to manipulate 
+#' @param vars character, names of columns to add label attribute
+#' @param labels character, labels to append to vars
+#' @return data.frame
+#' @examples 
+#' lbl <- c('Sepal Length','Sepal Width','Petal Length','Petal Width','Flower Species')
+#' 
+#' iris_labs <- iris%>%label(vars=names(iris),labels = lbl)
+#' 
+#' @rdname label
+#' @export 
+#' @author Jonathan Sidi
 label <- function(.data,vars,labels){
   
   IDX <- match(vars,names(.data))
