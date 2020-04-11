31ba1cf4b4dcbad1fc957e42f199fdb68f05a005
Fodil Ihaddaden <ihaddaden.fodeil@gmail.com>
2020-04-11 18:34:58 +0100

    Added the functions easy_bar_mean, easy_bar_median and easy_range. I've also tested the functions using the vdiffr package and validated them

--- R/easy_bar.R
+++ R/easy_bar.R
@@ -1,10 +1,11 @@
 #' Easy mean bar plot
 #'
-#' @description Create a bar plot that displays the mean of a numeric variable according to the values of a factor variable (factor in a statistical sense, it doesn't need to be of a  factor class)
-#' @md
-#' @param df the dataframe
+#' @description Create a bar plot that displays the mean of a numeric variable according to the values of a factor variable
+#' @param df the data frame
 #' @param fct_var the factor variable
 #' @param num_var the function will calculate the mean of num_var for each value of fct_var
+#' @param col the color of the bar plot borders
+#' @param fill the filling color of the bar plot
 #' @param teach print longer equivalent \code{\link[ggplot2]{ggplot2}}
 #' expression?
 #' @return a \code{\link[ggplot2]{ggplot2}} object
@@ -19,14 +20,120 @@
 
 
 
-easy_bar_mean <- function(df, fct_var, num_var, teach = FALSE) {
+easy_bar_mean <- function(df, fct_var, num_var, col = "brown1", fill = "brown4", teach = FALSE) {
 
 
   if (teach) {message("easy_bar_mean call can be substituted with:")
-    message(strwrap('ggplot(df, aes(reorder(fct_var, -num_var), num_var)) +
-    geom_bar(stat = "summary", fun = "mean") +
-      xlab("") +
-      xlab("mean")
+    message(strwrap(
+    '
+    ggplot(df, aes(forcats::fct_reorder(factor(fct_var), -num_var, .fun = mean), -num_var)) +
+      geom_bar(stat = "summary", fun = "mean", color = "brown1", fill = "brown4") +
+      ylab("mean") +
+      xlab("")
+
+      ',
+      width = 80,
+      exdent = 2,
+      prefix = "\n",
+      initial = ""
+    ))}
+
+
+    ggplot(df, aes(forcats::fct_reorder(factor({{fct_var}}), {{num_var}}, .fun = mean), {{num_var}})) +
+      geom_bar(stat = "summary", fun = "mean", color = col, fill = fill) +
+      ylab("mean") +
+      xlab("")
+
+
+
+  }
+
+
+#' Easy median bar plot
+#'
+#' @description Create a bar plot that displays the median of a numeric variable according to the values of a factor variable
+#' @md
+#' @param df the data frame
+#' @param fct_var the factor variable
+#' @param num_var the function will calculate the median of num_var for each value of fct_var
+#' @param col the color of the bar plot borders
+#' @param fill the filling color of the bar plot
+#' @param teach print longer equivalent \code{\link[ggplot2]{ggplot2}}
+#' expression?
+#' @return a \code{\link[ggplot2]{ggplot2}} object
+#' @export
+#' @author Mohamed El Fodil Ihaddaden
+#' @examples
+#' library(ggplot2)
+#' easy_bar_median(df = mpg, fct_var = class, num_var = hwy)
+#'
+#'
+#'
+
+easy_bar_median <- function(df, fct_var, num_var, col = "brown1", fill = "brown4", teach = FALSE) {
+
+
+  if (teach) {message("easy_bar_median call can be substituted with:")
+    message(strwrap(
+      '
+    ggplot(df, aes(forcats::fct_reorder(factor(fct_var), -num_var, .fun = median), -num_var)) +
+      geom_bar(stat = "summary", fun = "median", color = "brown1", fill = "brown4") +
+      ylab("median") +
+      xlab("")
+
+      ',
+      width = 80,
+      exdent = 2,
+      prefix = "\n",
+      initial = ""
+    ))}
+
+
+  ggplot(df, aes(forcats::fct_reorder(factor({{fct_var}}), {{num_var}}, .fun = median), {{num_var}})) +
+    geom_bar(stat = "summary", fun = "median", color = col, fill = fill) +
+    xlab("") +
+    ylab("median")
+
+
+
+}
+
+
+
+#' Easy range bar plot
+#'
+#' @description Create a range plot in the for of error bars that displays the range of a numeric variable according to the values of a factor variable
+#' @md
+#' @param df the data frame
+#' @param fct_var the factor variable
+#' @param num_var the function will calculate the range of num_var for each value of fct_var
+#' @param col the color of the range bar plot borders
+#' @param teach print longer equivalent \code{\link[ggplot2]{ggplot2}}
+#' expression?
+#' @return a \code{\link[ggplot2]{ggplot2}} object
+#' @export
+#' @author Mohamed El Fodil Ihaddaden
+#' @examples
+#' library(ggplot2)
+#' easy_range(df = mpg, fct_var = class, num_var = hwy)
+#'
+#'
+#'
+
+easy_range <- function(df, fct_var, num_var, col = "brown1", teach = FALSE) {
+
+
+  if (teach) {message("easy_range call can be substituted with:")
+    message(strwrap(
+      '
+
+  ggplot(df, aes(forcats::fct_reorder(factor(fct_var), num_var, .fun = max), num_var)) +
+    stat_summary(fun.data = so.summary, geom = "errorbar",  colour = "brown1", size = 2) +
+    xlab("") +
+    ylab("range")
+
+
+
 
       ',
       width = 80,
@@ -37,69 +144,14 @@ easy_bar_mean <- function(df, fct_var, num_var, teach = FALSE) {
 
 
 
-    ggplot({{df}}, aes(reorder({{fct_var}}, -{{num_var}}), {{num_var}})) +
-        geom_bar(stat = "summary", fun = "mean") +
-        ylab("mean") +
-        xlab("")
 
-
-  }
-
-
-
-
-
-#' Easy median bar plot
-#'
-#' @description Create a bar plot that displays the median of a numeric variable according to the values of a factor variable (factor in a statistical sense, it doesn't need to be a  factor class)
-#' @md
-#' @param df the dataframe
-#' @param fct_var the factor variable
-#' @param num_var the function will calculate the mean of num_var for each value of fct_var
-#' @param teach print longer equivalent \code{\link[ggplot2]{ggplot2}}
-#' expression?
-#' @return a \code{\link[ggplot2]{theme}} object
-#' @export
-#' @author Mohamed El Fodil Ihaddaden
-#' @examples
-#' library(ggplot2)
-#' easy_bar_median(df = mpg, fct_var = class, num_var = hwy)
-#'
-#'
-#'
-
-
-
-
-easy_bar_median<- function(df, fct_var, num_var, teach = FALSE) {
-
-
-
-
-  return(ggplot(df, aes(reorder({{fct_var}}, -{{num_var}}), {{num_var}})) +
-      geom_bar(stat = "summary", fun = "median") +
-      ylab("median") +
-      xlab(""))
-
-
-  if (teach) {
-    message("easy_bar_mean call can be substituted with:")
-    message(strwrap('ggplot(df, aes(reorder(fct_var, -num_var), num_var)) +
-    geom_bar(stat = "summary", fun.y = "median") +
-      xlab("") +
-      xlab("mean")
-
-      ',
-      width = 80,
-      exdent = 2,
-      prefix = "\n",
-      initial = ""
-    ))
-
-  }
-
+  ggplot(df, aes(forcats::fct_reorder(factor({{fct_var}}), {{num_var}}, .fun = max), {{num_var}})) +
+    stat_summary(fun.data = so.summary,geom = "errorbar",  colour = "brown1", size = 2) +
+    xlab("")+
+    ylab("range")
 
 
 }
+
 
 
--- tests/testthat/test-easy_bar.R
+++ tests/testthat/test-easy_bar.R
@@ -1,20 +1,65 @@
-library(ggeasy)
-library(tidyverse)
+context("barplots")
 
 
+# testing the easy_bar_mean() function #################
 
-test_that("easy_remove_legend_title works", {
+testthat::test_that("easy bar mean draw correctly", {
 
-
-  easy_res <- easy_bar_mean(mpg, class, hwy)
-
-
-
-  hard_res <- ggplot(mpg, aes(reorder(class, -hwy), hwy)) +
-    geom_bar(stat = "summary", fun = "mean") +
-    xlab("") +
-    xlab("mean")
+  bar_mean_easy <- easy_bar_mean(mtcars, am, mpg)
 
 
-  expect_equal(easy_res, hard_res)
+  bar_mean_ggplot <- ggplot(mtcars, aes(forcats::fct_reorder(factor(am), mpg, .fun = mean), mpg)) +
+    geom_bar(stat = "summary",
+             fun = "mean",
+             col = "brown1",
+             fill = "brown4") +
+    ylab("mean") +
+    xlab("")
+
+  vdiffr::expect_doppelganger("ggeasy easy_bar_mean", bar_mean_easy)
+  vdiffr::expect_doppelganger("ggplot2 barplot mean", bar_mean_ggplot)
+
 })
+
+# testing the easy_bar_median() function #################
+
+testthat::test_that("easy bar median draw correctly", {
+
+  bar_median_easy <- easy_bar_median(mtcars, am, mpg)
+
+
+  bar_median_ggplot <- ggplot(mtcars, aes(forcats::fct_reorder(factor(am), mpg, .fun = median), mpg)) +
+    geom_bar(stat = "summary",
+             fun = "median",
+             col = "brown1",
+             fill = "brown4") +
+    ylab("median") +
+    xlab("")
+
+  vdiffr::expect_doppelganger("ggeasy easy_bar_median", bar_median_easy)
+  vdiffr::expect_doppelganger("ggplot2 barplot median", bar_median_ggplot)
+
+})
+
+
+# testing the easy_bar_median() function #################
+
+
+
+
+testthat::test_that("easy range bar draw correctly", {
+
+  easy_range <- easy_range(mtcars, am, mpg)
+
+
+  ggplot_range <- ggplot(mtcars, aes(forcats::fct_reorder(factor(am), mpg, .fun = max), mpg)) +
+    stat_summary(fun.data = so.summary,geom = "errorbar",  colour = "brown1", size = 2) +
+    xlab("")+
+    ylab("range")
+
+  vdiffr::expect_doppelganger("easy range", easy_range)
+  vdiffr::expect_doppelganger("ggplot2 range", ggplot_range)
+
+})
+
+
